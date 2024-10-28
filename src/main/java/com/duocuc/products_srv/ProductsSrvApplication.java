ebe8ebeebb94ee6e927a50bd7470fe2de155b039
gustavoromerocl <gandresrp@gmail.com>
2024-10-28 00:41:50 -0300

    feat: create base project

---
+++ src/main/java/com/duocuc/products_srv/ProductsSrvApplication.java
@@ -0,0 +1,13 @@
+package com.duocuc.products_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duocuc/products_srv/ProductsSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocuc.products_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
